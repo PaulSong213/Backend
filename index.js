7288b8a032769ab4fcedb703ea60106bd0ed1071
AlertoBarangay <songaliajohnpaul@gmail.com>
2021-09-03 18:46:48 +0800

    add send message

--- index.js
+++ index.js
@@ -9,18 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const APP_ID = '94MguznMMeCb5ip5KpcMnGC8k4XzuxXL';
+const APP_SECRET = '9187c95f19058452262e5846fadaab53a254a3e11de41dea6a438edc402adc00';
+const shortCode = "21585411";
+const shortCodeLast4Digit = "5411";
+
 app.get('/', (req, res) => {
-    const APP_ID = '94MguznMMeCb5ip5KpcMnGC8k4XzuxXL';
-    const APP_SECRET = '9187c95f19058452262e5846fadaab53a254a3e11de41dea6a438edc402adc00';
     const code = req.query.code;
     if (!code) {
         res.status(403).send({ message: 'Invalid request.'});
     };
-// Construct our POST url.
+    // Construct our POST url.
     const globe_labs_url = `https://developer.globelabs.com.ph/oauth/access_token?app_id=${APP_ID}&app_secret=${APP_SECRET}&code=${code}`;
 
-// Send it to Globe Labs!
-console.log(globe_labs_url);
+    // Send it to Globe Labs!
+    console.log(globe_labs_url);
     axios.post(globe_labs_url, {})
     .then((response) => {
         const access_token = response.data.access_token;
@@ -40,9 +43,46 @@ console.log(globe_labs_url);
     })
 });
 
-app.get('/', (req, res) => {
-  res.status(200).send('/index.html');
-})
+app.get('/send-message', (req, res) => {
+    const accessToken = req.query.accessToken;
+    const subscriberNumber = req.query.subscriberNumber;
+    const message = req.query.message || 'Test Message';
+
+    if (!accessToken || !subscriberNumber) {
+        res.status(403).send({ message: 'No accessToken or subscriberNumber given.'});
+    };
+    console.log(accessToken,subscriberNumber);
+    res.status(200).send({'message' : 'gets'});
+
+    // Then, we need to compose our payload that we will send to Globe Labs.
+    const payload = {
+        outboundSMSMessageRequest: {
+            outboundSMSTextMessage: {
+                message: message
+            },
+            senderAddress: shortCodeLast4Digit,
+            address: `+63${subscriberNumber}`
+        }
+    }
+
+    // Compose our url
+    const url = `https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/${shortCodeLast4Digit}/requests?access_token=${accessToken}`;
+
+    // Send the request via Axios.
+    axios.post(url, payload)
+    .then(() => {
+        // Success!
+        res.send(`Message sent!`);
+    })
+    .catch((err) => {
+        // If there was an error, we should log it.
+        console.error(err);
+        res.sendStatus(500);
+    })
+
+});
+
+
 
 const port = process.env.PORT || 80;
 app.listen(port, () => {
