64b097b828c27db59a4e27d82423bd20550dd4b0
PaulSong213 <songalia@gmail.com>
2022-09-16 08:43:27 +0800

    first commit

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const APP_ID = '94MguznMMeCb5ip5KpcMnGC8k4XzuxXL';
-const APP_SECRET = '9187c95f19058452262e5846fadaab53a254a3e11de41dea6a438edc402adc00';
-const shortCode = "21585411";
-const shortCodeLast4Digit = "5411";
+const APP_ID = 'X6A6CGdBaetq5cdA7xTBnLtKG69ACLXA';
+const APP_SECRET = '75e9497563247a9ef69c0521af0327bef6bb64973518d30c08b2a8c954295afe';
+const shortCode = "21667138";
+const shortCodeLast4Digit = "7138";
 
 app.get('/', (req, res) => {
     const code = req.query.code;
     if (!code) {
-        res.status(403).send({ message: 'Invalid request.'});
+        res.status(403).send({ message: 'Invalid request.' });
     };
     // Construct our POST url.
     const globe_labs_url = `https://developer.globelabs.com.ph/oauth/access_token?app_id=${APP_ID}&app_secret=${APP_SECRET}&code=${code}`;
@@ -25,22 +25,22 @@ app.get('/', (req, res) => {
     // Send it to Globe Labs!
     console.log(globe_labs_url);
     axios.post(globe_labs_url, {})
-    .then((response) => {
-        const access_token = response.data.access_token;
-        const subscriber_number = response.data.subscriber_number;
+        .then((response) => {
+            const access_token = response.data.access_token;
+            const subscriber_number = response.data.subscriber_number;
 
-        // Store this to the database!
-        console.log(access_token, subscriber_number);
-        res.status(200).send({ 
-          accessToken : access_token,
-          subscriberNumber : subscriber_number 
-        });
-    })
-    .catch((err) => {
-        // If there was an error, we should log it.
-        console.log(err);
-        res.status(500).send({ message: 'Internal Server Error'});
-    })
+            // Store this to the database!
+            console.log(access_token, subscriber_number);
+            res.status(200).send({
+                accessToken: access_token,
+                subscriberNumber: subscriber_number
+            });
+        })
+        .catch((err) => {
+            // If there was an error, we should log it.
+            console.log(err);
+            res.status(500).send({ message: 'Internal Server Error' });
+        })
 });
 
 app.get('/send-message', (req, res) => {
@@ -49,9 +49,9 @@ app.get('/send-message', (req, res) => {
     const message = req.query.message || 'Test Message';
 
     if (!accessToken || !subscriberNumber) {
-        res.status(403).send({ message: 'No accessToken or subscriberNumber given.'});
+        res.status(403).send({ message: 'No accessToken or subscriberNumber given.' });
     };
-    console.log(accessToken,subscriberNumber);
+    console.log(accessToken, subscriberNumber);
     //res.status(200).send({'message' : 'gets'});
 
     // Then, we need to compose our payload that we will send to Globe Labs.
@@ -70,15 +70,15 @@ app.get('/send-message', (req, res) => {
 
     // Send the request via Axios.
     axios.post(url, payload)
-    .then(() => {
-        // Success!
-        res.send(`Message sent!`);
-    })
-    .catch((err) => {
-        // If there was an error, we should log it.
-        console.error(err);
-        res.sendStatus(500);
-    })
+        .then(() => {
+            // Success!
+            res.send(`Message sent!`);
+        })
+        .catch((err) => {
+            // If there was an error, we should log it.
+            console.error(err);
+            res.sendStatus(500);
+        })
 
 });
 
